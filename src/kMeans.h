4e476421fc8638337c1052a7a45fab6b0b738ba4
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-08-15 22:36:13 +0200

    Created initial versions of kMeans class.

---
+++ src/kMeans.h
@@ -0,0 +1,14 @@
+#ifndef KMEANS_KMEANS_H
+#define KMEANS_KMEANS_H
+
+#include "lib/interfaces/clusterRelated/clusteringAlgorithms/i_distanceBasedClusteringAlgorithm.h"
+
+
+/**
+ *  Implementation of classic k-means (BIMSEC) algorithm.
+ */
+class kMeans : protected i_distanceBasedClusteringAlgorithm {
+
+};
+
+#endif //KMEANS_KMEANS_H
