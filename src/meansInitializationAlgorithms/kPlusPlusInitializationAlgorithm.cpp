982d3914c3078eb234c0e4c004b5942573f70058
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-10-21 16:45:53 +0200

    Added a function that counts clusters distances form means set.

--- src/meansInitializationAlgorithms/kPlusPlusInitializationAlgorithm.cpp
+++ src/meansInitializationAlgorithms/kPlusPlusInitializationAlgorithm.cpp
@@ -1,7 +1,70 @@
 #include "kPlusPlusInitializationAlgorithm.h"
+#include <cstdlib>
 
+kPlusPlusInitializationAlgorithm::kPlusPlusInitializationAlgorithm(clusterDistanceMeasurePtr clusterDistanceMeasure)
+: _clusterDistanceMeasure(clusterDistanceMeasure) {}
+
+
+/** Main method for selecting k means from given set of clusters according to k++ algorithm.
+ *
+ *  @param k - desired number of means
+ *  @param clusters - initial set of clusters
+ *  @return
+ */
 std::vector<clusterPtr> kPlusPlusInitializationAlgorithm::selectInitialMeans(unsigned int k,
                                                                              std::vector<clusterPtr> clusters) {
+  std::vector<clusterPtr> means;
 
-  return std::vector<clusterPtr>();
+  // Randomly select first mean
+  int newMeanIndex = 0;
+  newMeanIndex = rand() % clusters.size();
+
+  // Add it to the means and remove from set of considered clusters
+  means.push_back(clusters[newMeanIndex]);
+  clusters.erase(clusters.begin() + newMeanIndex);
+
+  std::vector<double> clustersDistanceFromMeans;
+
+  // Repeat until desired number of means is achieved
+  while(means.size() < k) {
+    clustersDistanceFromMeans = countClustersDistanceFromMeans(means, clusters);
+
+  }
+
+
+
+
+  return means;
 }
+
+/** A method that counts distances of each cluster from clusters set from means set. It should be noted that distances
+ *  indexes corresponds to cluster indexes.
+ *
+ *  @brief A method that counts distances of each cluster from clusters set from means set.
+ *
+ *  @param means - current means
+ *  @param clusters  - other clusters
+ *  @return distances of each clusters from means set
+ */
+std::vector<double> kPlusPlusInitializationAlgorithm::countClustersDistanceFromMeans(std::vector<clusterPtr> means,
+                                                                                     std::vector<clusterPtr> clusters) {
+  std::vector<double> distances;
+
+  double minClusterMeanDistance = 0, currentDistance = 0;
+
+  for(clusterPtr cluster : clusters)
+  {
+    minClusterMeanDistance = 0;
+
+    for(int i = 0; i < means.size(); ++i)
+    {
+      currentDistance = _clusterDistanceMeasure->getClustersDistance(cluster, means[i]);
+      minClusterMeanDistance = currentDistance < minClusterMeanDistance ? currentDistance : minClusterMeanDistance;
+    }
+
+    distances.push_back(minClusterMeanDistance);
+  }
+
+  return distances;
+}
+
--- src/meansInitializationAlgorithms/kPlusPlusInitializationAlgorithm.h
+++ src/meansInitializationAlgorithms/kPlusPlusInitializationAlgorithm.h
@@ -2,13 +2,19 @@
 #define KMEANS_KPLUSPLUSINITIALIZATIONALGORITHM_H
 
 #include "i_meansInitializationAlgorithm.h"
+#include "../lib/interfaces/clusterRelated/i_clusterDistanceMeasure.h"
 
 class kPlusPlusInitializationAlgorithm : public i_meansInitializationAlgorithm{
   public:
-    kPlusPlusInitializationAlgorithm();
+    kPlusPlusInitializationAlgorithm(clusterDistanceMeasurePtr clusterDistanceMeasure);
 
     std::vector<clusterPtr> selectInitialMeans(unsigned int k, std::vector<clusterPtr> clusters) override;
+
   protected:
+    clusterDistanceMeasurePtr _clusterDistanceMeasure;
+
+    std::vector<double> countClustersDistanceFromMeans(std::vector<clusterPtr> means, std::vector<clusterPtr> clusters);
+
 };
 
 #endif //KMEANS_KPLUSPLUSINITIALIZATIONALGORITHM_H
